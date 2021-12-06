8a3b117059fe7221c02e912a2dfb7d74e9f57c54
Joke <jokekalter@gmail.com>
2021-12-06 22:38:22 +0100

    AOC-2021: docstrings and some simplifying

--- src/day_5_2.py
+++ src/day_5_2.py
@@ -7,33 +7,33 @@ class HydrothermalVentureMap:
         self.allow_vertical = allow_vertical
         self.field_grid = [[0] * 1000 for i in range(0, 1001)]
 
-    def add_vector_to_grid(self, start, end):
-        # high - low
-        if start[0] == end[0]:
-            low, high = (start[1], (end[1] + 1)) if start[1] < end[1] \
-                else (end[1], (start[1] + 1))
-            for y in range(low, high):
-                self.field_grid[start[0]][y] = self.field_grid[start[0]][y] + 1
-        if start[1] == end[1]:
-            low, high = (start[0], (end[0] + 1)) if start[0] < end[0] \
-                else (end[0], (start[0] + 1))
-            for x in range(low, high):
-                self.field_grid[x][start[1]] = self.field_grid[x][start[1]] + 1
-        if (start[0] != end[0]) and (start[1] != end[1]):
-            x_direction = "up" if start[0] < end[0] else "down"
-            y_direction = "up" if start[1] < end[1] else "down"
+    def add_vector_to_grid(self, start_point: tuple, end_point: tuple):
+        if start_point[0] == end_point[0]:
+            low, high = sorted([start_point[1], end_point[1]])
+            for y in range(low, high + 1):
+                self.field_grid[start_point[0]][y] += 1
+        if start_point[1] == end_point[1]:
+            low, high = sorted([start_point[0], end_point[0]])
+            for x in range(low, high + 1):
+                self.field_grid[x][start_point[1]] += 1
+        if (start_point[0] != end_point[0]) and (start_point[1] != end_point[1]):
+            x_direction_up = start_point[0] < end_point[0]
+            y_direction_up = start_point[1] < end_point[1]
 
-            length = abs(start[0] - end[0])
+            length = abs(start_point[0] - end_point[0])
             steps = 0
-            diagonal_x = start[0]
-            diagonal_y = start[1]
+            diagonal_x = start_point[0]
+            diagonal_y = start_point[1]
             while steps != (length + 1):
-                self.field_grid[diagonal_x][diagonal_y] = self.field_grid[diagonal_x][diagonal_y] + 1
-                diagonal_x = diagonal_x + 1 if x_direction == "up" else diagonal_x - 1
-                diagonal_y = diagonal_y + 1 if y_direction == "up" else diagonal_y - 1
+                self.field_grid[diagonal_x][diagonal_y] += 1
+                diagonal_x = diagonal_x + 1 if x_direction_up else diagonal_x - 1
+                diagonal_y = diagonal_y + 1 if y_direction_up else diagonal_y - 1
                 steps += 1
 
     def count_more_than_two(self):
+        """
+        Count the number of points/coordinates that more than one vector crosses
+        """
         total = 0
         for line in self.field_grid:
             for number in line:
@@ -48,7 +48,6 @@ def process(input_list: List) -> int:
     :param input_list:
     :return:
     """
-    total = 0
     venture_map = HydrothermalVentureMap()
     for vector in input_list:
         start_coordinates, end_coordinates = vector.split(' -> ')
@@ -64,3 +63,4 @@ if __name__ == '__main__':
     print(lines[0:10])
     output = process(lines)
     print(f'Output: {output}')
+    print(f'Output still correct: {int(output) == 21305}')
